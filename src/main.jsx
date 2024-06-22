d45a53aee22778cb7e4908a896ed6a0c6e41efeb
Martin Reyes <martin.reyes@unmsm.edu.pe>
2024-06-22 18:03:56 -0500

    Terminado la interfaz de Detalles.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
 import './index.css'
+import { Detalles } from './page/Detalles.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Detalles />
   </React.StrictMode>,
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ export default {
   ],
   theme: {
     extend: {
+      colors: {
+        'ri-red': '#f03949',
+        'ri-white': '#f8faff',
+        'ri-blue': '#29293e',
+        'ri-text': '#a7a9af'
+      },
       fontFamily: {
         sans: ['Roboto', 'sans-serif'], // Define Roboto como la fuente sans-serif por defecto
       },
