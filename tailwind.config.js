5c6e7c481377c5dff9fcd374c58f6c6d123b7d52
Jmond544 <jmondalgotapia@gmail.com>
2024-06-22 00:20:18 -0500

    Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'], // Define Roboto como la fuente sans-serif por defecto
+      },
+    },
+  },
+  plugins: [],
+}
+
